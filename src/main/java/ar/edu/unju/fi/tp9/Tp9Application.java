abe846ab346bafdcd87ed7edd97ca16c4e467cc3
21Enzo17 <enzo.meneghini@fi.unju.edu.ar>
2023-10-25 12:50:00 -0300

    Configuracion Inicial del proyecto Se realizo la configuracion inicial del proyecto.

---
+++ src/main/java/ar/edu/unju/fi/tp9/Tp9Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp9Application.class, args);
+	}
+
+}
