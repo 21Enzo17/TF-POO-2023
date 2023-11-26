1b65763432c0eb67973a79320f60690e0d284822
21Enzo17 <enzo.meneghini@fi.unju.edu.ar>
2023-11-26 18:00:13 -0300

    Se quito un import

--- src/main/java/ar/edu/unju/fi/tp9/controller/LibroResource.java
+++ src/main/java/ar/edu/unju/fi/tp9/controller/LibroResource.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
